a2920d34ca40062d6b5e3e5dcec4415eb96e4df8
Oleg Dudar <oleg.dudar@gmail.com>
2018-12-25 20:54:10 +0200

    Add timeit decorator to measure method execution time

--- performance_measure.py
+++ performance_measure.py
@@ -1,6 +1,22 @@
 import contextlib
+import time
+
 from selenium.webdriver.support.expected_conditions import staleness_of
 from selenium.webdriver.support.wait import WebDriverWait
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('%r  %2.2f ms' % \
+                  (method.__name__, (te - ts) * 1000))
+        return result
+    return timed
 
 def detectTimings(name, driver):
     navigationStart = driver.execute_script("return window.performance.timing.navigationStart")
@@ -19,3 +35,5 @@ def wait_for_page_load(self, timeout=200):
     old_page = self.find_element_by_tag_name('html')
     yield
     WebDriverWait(self, timeout).until(staleness_of(old_page))
+
+
